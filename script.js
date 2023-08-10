73ed1101c265af80685f75d623795912ffe45111
fran0770 <franmowat33@gmail.com>
2023-08-10 12:06:27 +0100

    added comparison between player and computer choices to determine winner

--- script.js
+++ script.js
@@ -18,5 +18,18 @@ let getComputerChoice = () => {
     return computerSelection 
 }
 
-let computer_choice = getComputerChoice()
-console.log(computer_choice)
+let playGame = (player_selection, computer_selection) => {
+    player_selection = player_selection.toLowerCase()
+    let result;
+    if (player_selection === computer_selection){
+        result = `Both players selected ${player_selection} and therefore the game was a draw.`;
+    } else if ((player_selection === "scissors" && computer_selection === "rock") || (player_selection === "rock" && computer_selection === "paper") || (player_selection === "paper" && computer_selection === "scissors")){
+        result = `You selected ${player_selection}, but the computer selected ${computer_selection}, so the computer wins.`;
+    } else {
+        result = `The computer selected ${computer_selection} and you selected ${player_selection}, so you won!`;
+    }
+    return result;
+}
+
+let computer_choice = getComputerChoice();
+console.log(playGame("ROCK", computer_choice));
