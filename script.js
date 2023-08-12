a94c84d44ad7776c1b87b9181f0e395f999e743b
fran0770 <franmowat33@gmail.com>
2023-08-12 08:13:04 +0100

    created basic game of 1 round

--- script.js
+++ script.js
@@ -19,17 +19,26 @@ let numToChoice = (num) => {
             choice = "error when generating choice"
             break;
     }
-    console.log(choice)
-    return choice 
+    return choice; 
 }
 
 let computerChoice = () => {
     return numToChoice(generateRand());
 }
 
-let gameOutcome = (player_selection, computer_selection) => {
+let accessUserChoice = () => {
+    document.getElementById("rock").onclick = gameOutcome;
+    document.getElementById("paper").onclick = gameOutcome;
+    document.getElementById("scissors").onclick = gameOutcome;
+}
+
+let gameOutcome = (e) => {
+    player_selection = e.target.id
+    computer_selection = computerChoice();
+
     let result;
     let winner;
+
     if (player_selection === computer_selection){
         result = `Both players selected ${player_selection} and therefore the game was a draw.`;
         winner = false;
@@ -40,39 +49,39 @@ let gameOutcome = (player_selection, computer_selection) => {
         result = `The computer selected ${computer_selection} and you selected ${player_selection}, so you won!`;
         winner = "user";
     }
-    return [result, winner];
+    
+    document.getElementById("outcome").innerHTML = result;
+
+    return [result, winner]
 }
 
 let multiRounds = () => {
+    console.log(getUserChoice());
     let user_score = 0;
     let computer_score = 0;
-
     for (roundNumber=1; roundNumber<6; roundNumber++){
-        document.getElementsByTagName("h1").innerHTML = `Round ${roundNumber}`
-
-        //let user_choice = document.getElementById("user_selection").onclick.innerHTML;
-        console.log(user_choice);
-
-        game_return = document.getElementById("rock").addEventListener("click", gameOutcome("rock", getComputerChoice()));
-        game_return = document.getElementById("paper").addEventListener("click", gameOutcome("paper", getComputerChoice()));
-        game_return = document.getElementById("scissors").addEventListener("click", gameOutcome("paper", getComputerChoice()));
+        document.getElementsByTagName("h1")[0].innerHTML = `Round ${roundNumber}`
+        if (getUserChoice()){
+            gameOutput = gameOutcome(getUserChoice(), computerChoice())
+        }
         
-        let result = game_return[0];
-        let winner = game_return[1];
-        document.getElementById("outcome").innerHTML = result;
+        result = gameOutput[0]
+        winner = gameOutput[1]
 
         if (winner === "user"){
             user_score ++;
         } else if (winner === "computer"){
             computer_score ++;
         } else{
-            pass;
+            user_score = user_score;
+            computer_score = computer_score;
         }
-        
-        document.getElementById("user_score").innerHTML =(` User score: ${user_score}`);
-        document.getElementById("computer_score").innerHTML =(` Computer score: ${computer_score}`);
+
+        document.getElementById("user_score").innerHTML =(`User score: ${user_score}`);
+        document.getElementById("computer_score").innerHTML =(`Computer score: ${computer_score}`);
     }
 
+    
     if (user_score > computer_score){
         console.log("Congradulations! You won the game.");
     } else if (user_score < computer_score){
@@ -82,4 +91,5 @@ let multiRounds = () => {
     }
 }
 
-multiRounds();
+//window.onload = multiRounds();
+window.onload = accessUserChoice();
